6ad31a1dffd33641278ff91701b30cb5dddc77e9
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-01-17 18:01:55 -0500

    dynamic Julia

--- mandelbrot/mandelbrot.js
+++ mandelbrot/mandelbrot.js
@@ -1,15 +1,17 @@
-
+var canvx = 1600;
+var canvy = 800;
 
 function setup() { 
-	createCanvas(800,800);
+	createCanvas(canvx,canvy);
+
 	pixelDensity(1);
 	loadPixels();
 	var maxIter = 100; 
-	for (var x = 0; x<width ; x++){
+	for (var x = 0; x<width/2 ; x++){
 		for(var y = 0; y<height ;y++){
 			
 
-			var a = map(x,0,width,-2,2);
+			var a = map(x,0,width/2,-2,2);
 			var b = map(y,0,height,-2,2);
 			
 			var ca = a;
@@ -22,6 +24,7 @@ function setup() {
 				var bb = 2*a*b; 
 				a = aa+ca;
 				b = bb+cb;
+				
 				if(a*a +b*b > 16){
 					break;
 				}
@@ -46,5 +49,76 @@ function setup() {
 
 	updatePixels();
 
+
+
 }
 
+
+
+function enCanvas(x,y){
+	if(x<=canvx/2 && y<=canvy && mouseX>=0 && mouseY >=0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+function mouseClicked() {
+  if(enCanvas(mouseX,mouseY)){
+  		drawJulia(mouseX,mouseY);
+  }
+}
+
+
+function drawJulia(px,py){
+	var maxIter = 100; 
+	px = map(px,0,width/2,-2,2);
+	py = -map(py,0,height,-2,2);
+	console.log(px,py);
+	// console.log(map(px-width/2,0,width/2,-2,2))
+	for (var x = width/2; x<width ; x++){
+		for(var y = 0; y<height ;y++){
+			
+
+			var a = map(x-width/2,0,width/2,-2,2);
+			var b = map(y,0,height,-2,2);
+			
+			
+			var n  = 0 ;
+
+			while(n<maxIter){
+				var aa = a*a - b*b;
+				var bb = 2*a*b; 
+				a = aa+px;
+				b = bb+py;
+				
+				if(a*a +b*b > 16){
+					
+					break;
+				}
+				n++
+			}	
+
+			if(n == maxIter){
+				n = 0;
+			}
+		
+			var bright = map(n, 0, maxIter, 0, 1);
+			bright = map(sqrt(bright),0,1,0,255)
+
+		
+			var pix = (x + y * width) * 4;
+			pixels[pix + 0] = bright;
+			pixels[pix + 1] = bright;
+			pixels[pix + 2] = bright;
+			pixels[pix + 3] = 255;
+		}
+	}
+
+	updatePixels();
+	stroke(255);
+	line(width/2,0,width/2,canvy);
+	console.log("done Julia");
+
+}
