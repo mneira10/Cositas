c223386bf42526ee827154b1752eb4cd66d64ccc
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-01-23 12:15:03 -0500

    auto generate circles

--- circles/circles.js
+++ circles/circles.js
@@ -1,24 +1,22 @@
 function Circle(x, y) {
   this.x = x;
   this.y = y;
-  this.r = 50;
+  this.r = random(20);
   this.growing = true;
   this.show = function() {
     stroke(255);
     noFill();
-    strokeWeight(1);
+    strokeWeight(2);
     ellipse(x, y, this.r * 2, this.r * 2);
 
   }
   this.grow = function() {
     this.r += 1;
   }
-  this.edges = function() {
-    return !(this.x + this.r > width || x - this.r < 0 || this.y + this.r > height || this.y - this.r < 0);
-  }
-  this.seToca = function(c) {
+  this.edges = function(c) {
     var ans = false;
-    for (i in c) {
+    // console.log(c.length);
+    for (i =0 ;i< c.length; i++) {
       var d = dist(this.x, this.y, c[i].x, c[i].y);
       if (d - 2 <= (this.r + c[i].r) && !(c[i] === this)) {
         ans = true;
@@ -26,8 +24,10 @@ function Circle(x, y) {
       }
       // console.log(c[i]===this , this.x, c[i].x);
     }
-    return ans;
+    // return ans;
+    return !(this.x + this.r > width || x - this.r < 0 || this.y + this.r > height || this.y - this.r < 0) && !ans;
   }
+
 
 
 }
--- circles/packing.js
+++ circles/packing.js
@@ -1,10 +1,18 @@
 var c = [];
-
+var n = 100;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(40);
-  c.push(new Circle(100, 100));
-  c.push(new Circle(200, 200));
+  // frameRate(40);
+  count = 0;
+  while(count<n){
+    x = random(windowWidth);
+    y = random(windowHeight);
+    r = random(50);
+    if(valid(x,y,r)){
+      c.push(new Circle(x,y,r));
+      count++;
+    }
+  }
   // c.push(new Circle(300, 300));
   // console.log(c.x)
   // console.log("hi");
@@ -13,17 +21,25 @@ function setup() {
 function draw() {
   background(0);
 
-  for (i in c) {
+  for (var i = 0; i < c.length; i++) {
     if (c[i].growing) {
       c[i].grow();
-      c[i].growing  = c[i].edges() && (!c[i].seToca(c));
+      c[i].growing = c[i].edges(c);
 
-      // c[i].show();
     }
     c[i].show();
-    console.log(i);
 
   }
+}
 
-
+function valid(x, y, r) {
+  var ans = true;
+  for (var i = 0; i < c.length; i++) {
+    var d = dist(x, y, c[i].x, c[i].y);
+    if(d+5<r+c[i].r){
+      ans = false;
+      break;
+    }
+  }
+  return ans;
 }
